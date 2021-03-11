2903b8152b01995cdb14f13f8ae65ce0693330c1
yaominguo <missgmy@yahoo.com>
2021-03-11 10:58:51 +0800

    createWebHistory添加base参数

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Layout from './layout.vue'
 import menus from './menu'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/my-view-components/'),
   strict: true,
   routes: [
     {
