3f99b3ea9cb4594a787b5ba50c3db7b5924a461c
yaominguo <missgmy@yahoo.com>
2021-03-11 10:38:21 +0800

    更新

--- src/router.ts
+++ src/router.ts
@@ -7,13 +7,13 @@ export const router = createRouter({
   strict: true,
   routes: [
     {
-      path: '/my-view-components',
+      path: '/',
       name: 'Main',
       component: Layout,
-      redirect: '/my-view-components/my-grid',
+      redirect: '/my-grid',
       children: menus.map((menu) => ({
         name: menu.name,
-        path: `/my-view-components/${menu.name}`,
+        path: `/${menu.name}`,
         component: menu.component,
       })),
     },
