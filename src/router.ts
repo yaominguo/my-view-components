96f40b124019d91a032b6b3f0743ff17aaf31d03
yaominguo <missgmy@yahoo.com>
2021-03-19 14:21:06 +0800

    打包

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from './layout.vue'
 import menus from './menu'
 
 export const router = createRouter({
-  history: createWebHistory('/my-view-components/'),
+  history: createWebHashHistory('/my-view-components/'),
   strict: true,
   routes: [
     {
